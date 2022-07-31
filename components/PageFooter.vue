9f9b63644b6163721297d1500c8a46d436ef7fbc
Nicolò Teseo <teseonicolo@gmail.com>
2022-07-31 13:50:00 +0200

    'footer'

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="footer-container">
         <p>&copy; 2022 NT Headphones All right reserved </p>
-        <p>
-            <i class="pi pi-instagram"></i>
-            <i class="pi pi-twitter"></i>
-            <i class="pi pi-github"></i>
+        <p> 
+            <a href="https://instagram.com/nyco_tina">
+                <i class="pi pi-instagram"></i>
+            </a>
+            <a href="https://twitter.com/TeseoNicolo">
+                <i class="pi pi-twitter"></i>
+            </a>
+            <a href="https://github.com/nicodev06">
+                <i class="pi pi-github"></i>
+            </a>
+            <a href="mailto:teseonicolo@gmail.com">
+                <i class="pi pi-google"></i>
+            </a>
         </p>
     </div>
 </template>
